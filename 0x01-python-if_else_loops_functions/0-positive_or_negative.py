e876351d157c44b306b66158a9d77cc4139b8ca4
Blaq Swan <mr.alangobryan@gmail.com>
2022-05-31 14:14:11 +0300

    positive or negative

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 import random
-n = random.randint(-10, 10)
-y = ['is positive', 'is negative', 'is zero']
+number = random.randint(-10, 10)
 
-print(f'{n} {y[0]}' if n > 0 else (f'{n} {y[1]}' if n < 0 else f'{n} {y[2]}'))
+
+def zero_positive_or_negative(n):
+    """
+    prints a number is positive, negative, or zero
+    """
+    if n == 0:
+        return f"{numbber:d} is zero"
+    elif n > 1:
+        return f"{number:d} is positive"
+    else:
+        return f"{number:d} is negative"
+
+
+if __name__ == "__main__":
+    print(zero_positive_or_negative(number))
