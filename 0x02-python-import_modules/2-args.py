82b4279888b56d9d4daacf834c2321eb6175b7d6
Blaq Swan <mr.alangobryan@gmail.com>
2022-06-02 13:23:39 +0300

    commandline arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from sys import argv as args
+
+
+def commandline_args():
+    if len(args) < 2:
+        print("{} arguments.".format(0))
+    elif len(args) == 2:
+        print("{} argument:".format(len(args[1:])))
+        for count, arg in enumerate(args[1:], start=1):
+            print("{}: {}".format(count, arg))
+    else:
+        print("{} arguments:".format(len(args[1:])))
+        for count, arg in enumerate(args[1:], start=1):
+            print("{}: {}".format(count, arg))
+
+
+commandline_args()
