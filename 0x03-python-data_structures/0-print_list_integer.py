8c1775d962aa43d16e2ff0449d0ab9d9a7da8401
Blaq Swan <mr.alangobryan@gmail.com>
2022-06-04 21:56:20 +0300

    newline print

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        for i in my_list:
-            print("{}".format(i))
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        print("{}".format(my_list[i]))
