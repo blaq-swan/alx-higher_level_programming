67350078a9d73d88e0e0abca6543b053d97400ca
Blaq Swan <mr.alangobryan@gmail.com>
2022-06-05 21:07:41 +0300

    You can't C me

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    my_list = [i for i in my_string]
-
-    for x in my_list:
-        if x in 'cC':
-            my_list.remove(x)
-
-    string = ''
-    for x in my_list:
-        string += x
-
-    return string
+    for x in my_string:
+        if x in "Cc":
+            my_string = my_string.split(x)
+            my_string = "".join(my_string)
+    return my_string
