fb8e8b0ed7a10ae2f41825319a732690cbd1b82b
Blaq Swan <mr.alangobryan@gmail.com>
2022-06-06 00:13:43 +0300

    odd or even

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    new_list = my_list[:]
-    for i in new_list:
-        if i % 2 == 0:
-            new_list[i] = True
+    new_list = []
+
+    for x in my_list:
+        if x % 2 == 0:
+            new_list.append(True)
         else:
-            new_list[i] = False
+            new_list.append(False)
     return new_list
