de0475e2a65871b80888c80d8a6c55fa05656aac
Mariam Alarape <mariamalarape@yahoo.com>
2022-06-09 02:06:40 +0300

    best student

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,4 +6,5 @@ def best_score(a_dictionary):
     keys, values = list(a_dictionary.keys()), list(a_dictionary.values())
     score = sorted(list(zip(keys, values)), key=lambda x: x[1], reverse=True)
 
-    return score[0][0]
+    keys = dict(score).keys()
+    return list(keys)[0]
