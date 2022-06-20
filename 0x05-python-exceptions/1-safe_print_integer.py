130b6a3beadbdc1bc0abeb8ded7d28f72597949e
Blaq Swan <mr.alangobryan@gmail.com>
2022-06-20 20:11:20 +0300

    prints an integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@
 def safe_print_integer(value):
     try:
         if value == int(value):
-            print("{}".format(value))
+            print("{:d}".format(value))
             return True
         else:
             return False
