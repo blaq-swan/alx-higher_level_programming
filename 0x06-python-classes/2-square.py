12b614a088d52231ac353b474e100cee468db490
Blaq Swan <mr.alangobryan@gmail.com>
2022-06-21 23:25:16 +0300

    defines a based on 1-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        """
+        Constructor.
+
+        Args:
+            size: the length of a square
+
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: is size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
