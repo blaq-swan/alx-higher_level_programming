b22eee6aca41136f7e376204cf2bc63aae67c62c
Blaq Swan <mr.alangobryan@gmail.com>
2022-06-22 01:02:27 +0300

    coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,6 +5,9 @@
 class Square:
     """Defines a square"""
     def __init__(self, size=0, position=(0, 0)):
+        """
+        Constructor
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
