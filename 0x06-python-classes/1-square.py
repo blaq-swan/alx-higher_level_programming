daeca0dced43d0dcda3a51d0e67d23005ca7444f
Blaq Swan <mr.alangobryan@gmail.com>
2022-06-21 23:03:55 +0300

    defines a square based on 0-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: the length of a square
+        """
+        self.__size = self
