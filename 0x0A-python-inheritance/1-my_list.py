5e79dff08b339fc7fbf165f94b9a8d7df80d0e51
Blaq Swan <mr.alangobryan@gmail.com>
2022-07-04 23:58:03 +0300

    prints sorted list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,19 +8,6 @@ class MyList(list):
     """
 
     def print_sorted(self):
-        """prints sorted self which is a list
-
-        Raises:
-            TypeError:
-                self must be a list
-                self must have integers only
-        """
-
-        if not isinstance(self, list):
-            raise TypeError("self must be a list")
-
-        for x in self:
-            if not isinstance(x, (int)):
-                raise TypeError("self must have integers only")
+        """prints sorted self which is a list"""
 
         print(sorted(self))
