c528766304ec31b21e8993d3887f282ac5e98cd0
Blaq Swan <mr.alangobryan@gmail.com>
2022-07-05 01:16:23 +0300

    instance of a class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,6 +6,6 @@ def is_same_class(obj, a_class):
     """Checks if an object is an instance of a class
     Args:
         obj: object to be checked
-        a_class: class reference 
+        a_class: class reference
     """
     return True if type(obj) == a_class else False
