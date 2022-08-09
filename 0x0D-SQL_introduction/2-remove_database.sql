9c524e9ffdc1ee9057c5b2a520abddb7ec650c49
Blaq Swan <mr.alangobryan@gmail.com>
2022-08-09 22:32:45 +0300

    deletes database if existing

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -2,4 +2,4 @@
 -- The script should not fail if the database does not exist.
 -- You are not allowed to use the SELECT or SHOW statements
 
-DROP DATABASE IF NOT EXISTS hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
