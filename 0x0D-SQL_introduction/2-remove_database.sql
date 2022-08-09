80a946094849eccfa7e1e8363193df00313fb631
Blaq Swan <mr.alangobryan@gmail.com>
2022-08-09 22:31:18 +0300

    deletes database ig not existing

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- Deletes the database hbtn_0c_0 in your MySQL server.
+-- The script should not fail if the database does not exist.
+-- You are not allowed to use the SELECT or SHOW statements
+
+DROP DATABASE IF NOT EXISTS hbtn_0c_0;
