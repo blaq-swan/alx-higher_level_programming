ece5c9f5da3b889030b791b498899e72b42aee7f
Blaq Swan <mr.alangobryan@gmail.com>
2022-09-11 22:28:44 +0300

    list states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     db = md.connect(host="localhost", port=3306,
                     user=args[1], passwd=args[2], db=args[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM state")
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
     [print(state) for state in cursor.fetchall()]
