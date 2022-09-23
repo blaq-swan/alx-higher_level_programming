d6e41222ecc3df9f17d9af5679e270095135815f
Alang'o Bryan <mr.alangobryan@gmail.com>
2022-09-23 15:28:49 -0700

    fixed errors

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""Script that fetches from a url"""
+""" Script that fetches from a url using urllib package"""
 
 import urllib.request as request
 
-
 if __name__ == "__main__":
-    with request.urlopen("https://alx-intranet.hbtn.io/status") as url:
-        page = url.read()
+    with request.urlopen('https://intranet.hbtn.io/status') as r:
+        html = r.read()
         print("Body response:")
-        print("\t- type: {}".format(type(page)))
-        print("\t- content: {}".format(page))
-        print("\t- utf8 content: {}".format(page.decode("utf-8")))
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""Shows the X-Request-Id of a request from a URL"""
+"""
+Python script that takes in a URL, sends a request to
+the URL and displays the value of the X-Request-Id variable
+found in the header of the response.
+"""
 
 import urllib.request as request
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    query = request.Request(url)
-    with request.urlopen(query) as page:
-        print(dict(page.headers).get("X-Request-Id"))
+    with request.urlopen(argv[1]) as r:
+        print(r.getheader("X-Request-Id"))
